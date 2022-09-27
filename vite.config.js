62ccc1c281120bbf706c0b92fd71b302a0eb2c42
Taz Jackson <tatjsn@users.noreply.github.com>
2022-09-27 23:44:23 +0100

    Use published package

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ module.exports = {
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './js/main.js'),
         css: resolve(__dirname, './css/main.css'),
       },
       output: {
-        entryFileNames: () => '[name].js',
         assetFileNames: () => '[name][extname]',
       },
     },
